8d77d6c8fecba93a57fcdf52aa180321158c2d89
楼宇祺 <louyuqi2008@163.com>
2018-10-26 17:56:50 +0800

    update route forward

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
@@ -13,10 +14,14 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+/**
+ * 应用级中间件在路由级中间件之后执行
+ */
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
--- controller/apiController.js
+++ controller/apiController.js
@@ -1,18 +1,30 @@
 var express=require('express'),
 		router=express.Router();
+var {URL}=require('url');
+var bodyParser = require('body-parser');
 // var JWT=require('jsonwebtoken'),
 // 		gConfig=require('../config/general')
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 router.use((req,res,next)=>{
-		// console.log(req);
+		console.log(req.body);
 		let forwardMap={
 				'/walk/api':'http://walk.zjutjh.com/qq',
 		}
-		let url=req.url;
-		if(forwardMap.hasOwnProperty(url)){
-
+		let url=req._parsedUrl;
+		console.log(url.pathname);
+		if(forwardMap.hasOwnProperty(url.pathname)){
+			let urlForward=forwardMap[url.pathname];
+				if(url.search){
+					urlForward=`${urlForward}${url.search}`
+				}
+				console.log(urlForward);
+				return res.status(200).send({url:urlForward})
 		}
 		return res.status(400).send({message:'该api未注册'})
 
 })
-
+// router.post('/',function (res,req) {
+// 		console.log(req);
+// })
 module.exports=router
