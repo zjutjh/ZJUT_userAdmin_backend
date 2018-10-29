e6a3fb72b7c5f48e08f54a0b9f0ae4a044d1a4f5
楼宇祺 <louyuqi2008@163.com>
2018-10-29 10:11:41 +0800

    update 1

--- controller/apiController.js
+++ controller/apiController.js
@@ -2,24 +2,40 @@ var express=require('express'),
 		router=express.Router();
 var {URL}=require('url');
 var bodyParser = require('body-parser');
+var request=require('request-promise-native');
 // var JWT=require('jsonwebtoken'),
 // 		gConfig=require('../config/general')
 router.use(bodyParser.json()); // for parsing application/json
 router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 router.use((req,res,next)=>{
-		console.log(req.body);
+		let body=req.body;
+		// console.log(req);
+		// console.log(req.body);
 		let forwardMap={
 				'/walk/api':'http://walk.zjutjh.com/qq',
 		}
 		let url=req._parsedUrl;
-		console.log(url.pathname);
+		// console.log(url.pathname);
 		if(forwardMap.hasOwnProperty(url.pathname)){
 			let urlForward=forwardMap[url.pathname];
-				if(url.search){
+				if(url.search){//如果有查询
 					urlForward=`${urlForward}${url.search}`
 				}
-				console.log(urlForward);
-				return res.status(200).send({url:urlForward})
+				let response;
+				request({
+						uri:urlForward,
+						method:req.method,
+						body:JSON.stringify(body),
+				}).then((reqx)=>{
+						// console.log(res)
+						response= res.status(200).send(reqx)
+				}).catch((err)=>{
+						// console.log(err);
+						response= res.status(err.statusCode).send(err.message)
+				})
+				return response;
+				// console.log(urlForward);
+
 		}
 		return res.status(400).send({message:'该api未注册'})
 
